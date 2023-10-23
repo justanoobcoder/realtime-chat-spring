29b18c32aa7e1c4bdf800a36e45a32699897264e
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-10-23 09:54:18 +0700

    use spring Value annotation to inject values

--- server/src/main/java/com/chat/server/config/WebSocketConfig.java
+++ server/src/main/java/com/chat/server/config/WebSocketConfig.java
@@ -1,23 +1,38 @@
 package com.chat.server.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${app.websocket.endpoint}")
+    private String websocketEndpoint;
+
+    @Value("${app.websocket.destination.prefix}")
+    private String destinationPrefix;
+
+    @Value("${app.websocket.broker.prefixes}")
+    private String[] messageBrokerPrefixes;
+
+    @Value("${app.websocket.allowed.origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket")
-                .setAllowedOrigins("http://127.0.0.1:5500");
+        registry.addEndpoint(websocketEndpoint)
+                .setAllowedOrigins(allowedOrigins);
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app")
-                .enableSimpleBroker("/topic", "/queue");
+        registry.setApplicationDestinationPrefixes(destinationPrefix)
+                .enableSimpleBroker(messageBrokerPrefixes);
     }
 }
