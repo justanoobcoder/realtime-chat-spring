5af2e15acdf069d4def5dc3868ce91396a14e701
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2024-02-01 11:53:37 +0700

    feat(util): add constants

---
+++ server/src/main/java/com/chat/server/util/Constants.java
@@ -0,0 +1,6 @@
+package com.chat.server.util;
+
+public class Constants {
+    public static final String USER_NOT_FOUND = "USER_NOT_FOUND";
+    public static final String USERNAME_ALREADY_EXISTS = "USERNAME_ALREADY_EXISTS";
+}
